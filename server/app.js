df7a2e8f001306d56043294b185da82c1bc4a0f1
Divya P <113757574+Divya4242@users.noreply.github.com>
2024-08-21 10:45:04 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -39,6 +39,11 @@ const CreateAllFolder = require("./config/uploadFolderCreateScript");
 /* Create All Uploads Folder if not exists | For Uploading Images */
 CreateAllFolder();
 
+app.use(cors({
+    origin: [ 'https://kfrontend.devopswithdivya.me', '*' ],
+    methods: 'GET,POST,PUT,DELETE'
+}));
+
 // Database Connection
 mongoose
   .connect(process.env.DATABASE, {
@@ -56,7 +61,7 @@ mongoose
 // Middleware
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
